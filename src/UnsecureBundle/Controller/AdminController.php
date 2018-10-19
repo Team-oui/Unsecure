c26980eed4bdd4beda1ae88f4eee617cff1d5684
Albert Krasniqi <albert.krasniqi@groupetrefle.com>
2018-10-19 14:10:01 +0200

    test mauvaise syntaxe

--- src/UnsecureBundle/Controller/AdminController.php
+++ src/UnsecureBundle/Controller/AdminController.php
@@ -2,19 +2,19 @@
 
 namespace UnsecureBundle\Controller;
 
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Excoijlklkeption\AccessDeniedHttpException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class AdminController extends Controller
+class AdminControlkc,xw:;xcw,:;xwcler extends Controller
 {
-    public function indexAction()
+    public function indexAccxwkwxlkcxwtion()
     {
         $user = $this->get('unsecure.session')->getUser();
         
         if ((null === $user) || !$user->getAdmin()) {
             throw new AccessDeniedHttpException('Vous n\'avez pas access à cette partie.');
         }
-        
+        <wx<w
         $em = $this->getDoctrine()->getManager();
         
         $users = $em->getRepository('UnsecureBundle:User')->findAllWith(true);
